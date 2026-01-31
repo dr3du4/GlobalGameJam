90f56f0c0eaebbc53ef3b0c0f0c18894f226079e
Jakub Grzyb <grzybjaku@gmail.com>
2026-01-31 17:53:24 +0100

    walls + movement

--- Assets/Scripts/Banner.cs
+++ Assets/Scripts/Banner.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 
 public class Banner : MonoBehaviour
 {
+    [SerializeField] private bool isGridWalkerBanner = false;
     void Update()
     {
-        Vector3 directionToCamera = GameManager.instance.mainCamera.transform.position - transform.position;
+        Vector3 pos = isGridWalkerBanner ? GameManager.instance.secondCamera.transform.position : GameManager.instance.mainCamera.transform.position;
+        Vector3 directionToCamera = pos - transform.position;
         directionToCamera.y = 0;
         transform.rotation = Quaternion.LookRotation(directionToCamera);
     }
--- Assets/Scripts/MovementSpine.cs
+++ Assets/Scripts/MovementSpine.cs
@@ -3,15 +3,24 @@ using UnityEngine;
 public class MovementSpine : AbstractPlayer
 {
     [SerializeField] private float speed = 5f;
+    [SerializeField] private Rigidbody rb;
+    private Vector2 moveInput;
     override protected void MyUpdate()
     {
-        Vector2 moveInput = GameManager.instance.inputActions.Player.Move.ReadValue<Vector2>();
+        moveInput = GameManager.instance.inputActions.Player.Move.ReadValue<Vector2>();
 
         if (GameManager.instance.isCableEnjoyerChosen)
         {
             SetWalking(moveInput.magnitude > 0.1f);
-            transform.Translate(new Vector3(moveInput.x, 0, moveInput.y) * speed * Time.deltaTime);
+            // transform.Translate(new Vector3(moveInput.x, 0, moveInput.y) * speed * Time.deltaTime, Space.World);
             UpdateAnimationDirection(moveInput);
         }
+    }
+
+    void FixedUpdate()
+    {
+        // Debug.Log($"Move Input: {moveInput}");
+        rb.MovePosition(rb.position + new Vector3(moveInput.x, 0, moveInput.y) * speed * Time.fixedDeltaTime);
+        rb.linearVelocity = Vector3.zero;
     }
 }
