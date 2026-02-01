f577fc815fa53829482f96dd90d534b932a22e77
Jakub Grzyb <grzybjaku@gmail.com>
2026-02-01 15:12:49 +0100

    music + exclam

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public static MusicManager I;
+    public AudioSource GameplayMusicSource;
+    public AudioSource MenuMusicSource;
+    public AudioSource looseMusicSource;
+    public AudioSource winMusicSource;
+
+    void Awake()
+    {
+        if (I == null)
+        {
+            I = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        PlayMenuMusic();
+    }
+
+    public void PlayMenuMusic()
+    {
+        if(!MenuMusicSource.isPlaying)
+        {
+            MenuMusicSource.Play();
+            GameplayMusicSource.Stop();
+        }
+    }
+
+    public void PlayGameplayMusic()
+    {
+        if (!GameplayMusicSource.isPlaying)
+        {
+            MenuMusicSource.Stop();
+            GameplayMusicSource.Play();
+        }
+    }
+
+    public void PlaySFX(AudioSource sfxSource)
+    {
+        if (!sfxSource.isPlaying)
+        {
+            sfxSource.Play();
+        }
+    }
+}
