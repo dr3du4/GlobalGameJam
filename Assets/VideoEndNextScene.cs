8d6d18c220cd3e28570e93742c83a758174f2cb8
Jan Krzyszkowski <jan.krzyszkowski2@gmail.com>
2026-02-01 15:10:50 +0100

    cutscneka

---
+++ Assets/VideoEndNextScene.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class VideoEndNextScene : MonoBehaviour
+{
+    [SerializeField] private VideoPlayer videoPlayer;
+    [SerializeField] private string nextSceneName; // wpisz nazwę sceny (dokładnie)
+
+    private void Awake()
+    {
+        if (!videoPlayer) videoPlayer = GetComponent<VideoPlayer>();
+    }
+
+    private void OnEnable()
+    {
+        videoPlayer.loopPointReached += OnVideoFinished;
+    }
+
+    private void OnDisable()
+    {
+        videoPlayer.loopPointReached -= OnVideoFinished;
+    }
+
+    private void Start()
+    {
+        // Dla URL / czasem też dla VideoClip warto prepare, żeby nie było czarnej klatki na start.
+        videoPlayer.Prepare();
+        videoPlayer.prepareCompleted += _ => videoPlayer.Play();
+    }
+
+    private void OnVideoFinished(VideoPlayer vp)
+    {
+        // zabezpieczenie
+        if (!string.IsNullOrEmpty(nextSceneName))
+            SceneManager.LoadScene(nextSceneName);
+        else
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
